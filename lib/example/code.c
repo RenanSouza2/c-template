e8307ad605083a23b569805ee758c487796db74a
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2025-04-16 17:14:04 -0300

    mid commit

--- lib/example/code.c
+++ lib/example/code.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "../macros/U64.h"
-#include "../macros/assert.h"
+#include "debug.h"
 #include "../../mods/clu/header.h"
 
-#include "debug.h"
+#include "../macros/U64.h"
+#include "../macros/assert.h"
 
 
 
